581f428db129b5feaddc28c789e7a1e79689332b
babaoriley <helenebrulin@live.fr>
2020-05-23 17:26:49 +0200

    template tests update

---
+++ tests/Queue_test.cpp
@@ -0,0 +1,18 @@
+#include "../Queue.hpp"
+#include "iostream"
+
+int main(void)
+{
+	std::cout << "Testing constructor" << std::endl;
+////
+	std::cout << std::endl;
+
+	std::cout << "Testing member functions" << std::endl;
+////
+	std::cout << std::endl;
+
+	std::cout << "Testing operators" << std::endl;
+///
+	std::cout << std::endl;
+
+}
